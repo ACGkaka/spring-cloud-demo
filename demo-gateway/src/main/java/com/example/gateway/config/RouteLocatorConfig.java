de38b8ac219350975e5ce41ba34a6464b09d8639
hjzhao <hjzhao@tsingsoft.com.cn>
2020-12-17 13:21:37 +0800

    完善注释

--- demo-gateway/src/main/java/com/example/gateway/config/RouteLocatorConfig.java
+++ demo-gateway/src/main/java/com/example/gateway/config/RouteLocatorConfig.java
@@ -17,15 +17,18 @@ public class RouteLocatorConfig {
 
     /**
      * 手动实现请求路径的重写，将localhost:1003 -> lb://demo-business
-     * @param builder
-     * @return
+     * @param builder RouteLocatorBuilder
+     * @return RouteLocator
      */
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
+                        // 用于匹配符合重写条件的请求
                         .predicate(exchange -> exchange.getRequest().getPath().subPath(0).toString().equals(("/")))
+                        // 将/路径重写为/index
                         .filters(f -> f.rewritePath("/", "/index"))
+                        // 请求demo-business服务的/index路径
                         .uri("lb://demo-business"))
                 .build();
     }
